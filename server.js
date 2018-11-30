9fc832c50478a0041972b8c2bc669ae4f03c2bfd
Piyush Sharma <piyushsharma08@outlook.com>
2018-11-30 06:15:24 +0530

    added project page

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ app.get('/bad',(req,res)=>{
         errorMessage:'unable to handle request'
     });
 });
+
+app.get('/projects',(req,res) =>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projects'
+    });
+});
+
 app.listen(port,() =>{
     console.log(`Server is up on port ${port}`);
 });
